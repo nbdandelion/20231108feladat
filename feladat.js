655c07202575b5c26fd1b71791bca020acc8de45
pitypang18 <lorinczistvan1@gmail.com>
2023-11-08 20:32:44 +0100

    Feladat2

--- feladat.js
+++ feladat.js
@@ -19,3 +19,48 @@ function KorKerTer(r) {
 console.log(VanENegativ([5, 12, 23, -5, 14]));
 console.log(VanENegativ([5, 12, 23, 14]));
 console.log(KorKerTer(4));
+function MinAuto(autok) {
+    var min = autok[0];
+    for (var i = 0; i < autok.length; i++) {
+        if (autok[i].hengerurtartalom < min.hengerurtartalom) {
+            min = autok[i];
+        }
+    }
+    return min;
+}
+function BenzinesDb(autok) {
+    var db = 0;
+    for (var i = 0; i < autok.length; i++) {
+        if (autok[i].benzinesE /*== true*/) {
+            db++;
+        }
+    }
+    return db;
+}
+var a1 = {
+    gyarto: "Opel",
+    tipus: "Corsa",
+    hengerurtartalom: 1400,
+    benzinesE: true
+};
+var a2 = {
+    gyarto: "VW",
+    tipus: "Golf",
+    hengerurtartalom: 1600,
+    benzinesE: false
+};
+var a3 = {
+    gyarto: "Audi",
+    tipus: "TT",
+    hengerurtartalom: 1800,
+    benzinesE: true
+};
+var a4 = {
+    gyarto: "Smart",
+    tipus: "Roadster",
+    hengerurtartalom: 900,
+    benzinesE: true
+};
+var autok = [a1, a2, a3, a4];
+console.log(MinAuto(autok));
+console.log(BenzinesDb(autok));
--- feladat.ts
+++ feladat.ts
@@ -26,3 +26,68 @@ function KorKerTer(r:number):[number, number]{
 console.log(VanENegativ([5, 12, 23, -5, 14]));
 console.log(VanENegativ([5, 12, 23, 14]));
 console.log(KorKerTer(4));
+
+interface Auto{
+    gyarto:string;
+    tipus:string;
+    hengerurtartalom:number;
+    benzinesE:boolean;
+}
+
+
+function MinAuto(autok:Auto[]):Auto{
+    var min:Auto = autok[0];
+
+    for(var i:number = 0; i < autok.length; i++){
+        if(autok[i].hengerurtartalom < min.hengerurtartalom){
+            min = autok[i];
+        }
+    }
+
+    return min;
+}
+
+function BenzinesDb(autok:Auto[]):number{
+    var db:number = 0;
+
+    for(var i:number = 0; i < autok.length; i++){
+        if(autok[i].benzinesE /*== true*/){
+            db++;
+        }
+    }
+
+    return db;
+}
+
+var a1:Auto = {
+    gyarto: "Opel",
+    tipus: "Corsa",
+    hengerurtartalom: 1400,
+    benzinesE: true
+};
+
+var a2:Auto = {
+    gyarto: "VW",
+    tipus: "Golf",
+    hengerurtartalom: 1600,
+    benzinesE: false
+};
+
+var a3:Auto = {
+    gyarto: "Audi",
+    tipus: "TT",
+    hengerurtartalom: 1800,
+    benzinesE: true
+};
+
+var a4:Auto = {
+    gyarto: "Smart",
+    tipus: "Roadster",
+    hengerurtartalom: 900,
+    benzinesE: true
+};
+
+var autok:Auto[] = [a1, a2, a3, a4];
+
+console.log(MinAuto(autok));
+console.log(BenzinesDb(autok));
